ad626a0ae1135b7ee831851d181fca022d8632f7
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-09-13 17:54:10 -0300

    Initial Commit

---
+++ lib/temperature.rb
@@ -0,0 +1,14 @@
+require "temperature/version"
+
+module Temperature
+  def self.say
+    "Hello World"
+  end
+
+  class Temperature
+    attr_accessor :value, :unit
+    def initialize(value, unit)
+      @value, @unit = value, unit
+    end
+  end
+end
